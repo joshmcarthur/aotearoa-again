c091c29850f6cbdbfa8e0a85567b5b6ed6d02a08
Josh McArthur <joshua.mcarthur@gmail.com>
2026-07-28 09:46:46 +1200

    Add helpers for extracting a handle from a social URL

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "social_handle_from_url extracts @handle from profile urls" do
+    assert_equal "@aotearoa.again", social_handle_from_url("https://www.instagram.com/aotearoa.again/")
+    assert_equal "@aotearoaagain", social_handle_from_url("https://www.facebook.com/aotearoaagain")
+  end
+
+  test "social_handle_from_url returns nil for blank or invalid urls" do
+    assert_nil social_handle_from_url("")
+    assert_nil social_handle_from_url("https://www.instagram.com/")
+    assert_nil social_handle_from_url("not a url")
+  end
+end
